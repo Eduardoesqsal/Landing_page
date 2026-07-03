b46789edff490e7cdac6ccd61a4e7d14ef9b9721
Tu Nombre <eduardoesquivelsa@gmail.com>
2026-07-03 15:44:37 -0600

    Configure Netlify deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  base: "/Landing_page/",
   plugins: [react(), tailwindcss()],
 })
